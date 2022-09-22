9710bc722407e72b01dbb60139a40b443628b459
Quentin Ikeno <ikenoq@gmail.com>
2022-09-21 17:48:17 -1000

    Write twoArrayObject function.

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,9 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+	// Use reduce on keys to create an object
+	return keys.reduce((result, key, i) => {
+		// Set value for each key in result to be the corresponding value in values or null
+		result[key] = values[i] || null;
+		return result;
+	}, {});
 }
