7ac51cf21a9b05ea0e60e2919cdfc09d62dda987
Quentin Ikeno <ikenoq@gmail.com>
2022-09-22 19:18:16 -1000

    Write countPairs function.

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, target) {
+	arr.sort((a, b) => a - b); // Sort the array
+	let count = 0; // Counter for pairs
+	// Use two pointers to check if the values sum to the target
+	let left = 0;
+	let right = arr.length - 1;
+	while (left < right) {
+		let sum = arr[left] + arr[right];
+		if (sum === target) {
+			count++;
+			left++;
+			right--;
+		} else if (sum < target) left++;
+		else right--;
+	}
+	return count;
 }
