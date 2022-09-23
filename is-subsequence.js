9a6d9039894a679d2fa4ff397af42b9c265f941a
Quentin Ikeno <ikenoq@gmail.com>
2022-09-22 19:03:51 -1000

    Write isSubsequence function."

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+	if (!str1) return true;
+	// Create pointers for str1 and str2
+	let i = 0;
+	let j = 0;
+	//Loop through str2
+	while (j < str2.length) {
+		// If str1[i] matches str2[j] increment i
+		if (str1[i] === str2[j]) i++;
+		// If we've incremented through the entirety of str1 it is a subsequence
+		if (i === str1.length) return true;
+		j++; // Increment j
+	}
+	return false;
 }
