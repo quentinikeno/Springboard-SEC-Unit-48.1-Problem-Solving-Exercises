8a7627e28b5ab6d9d93e9d2a807de50d800272a7
Quentin Ikeno <ikenoq@gmail.com>
2022-09-22 19:39:53 -1000

    Write longestFall function.

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,20 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(arr) {
+	if (arr.length === 0) return 0;
+	// variables to store count and the max count
+	let count = 1;
+	let maxCount = 0;
+
+	// loop through the array
+	for (let i = 0; i < arr.length; i++) {
+		// when the next value is smaller increase the count
+		if (arr[i + 1] < arr[i]) count++;
+		else {
+			// otherwise update the maxCount with the max of the current count and the maxCount
+			maxCount = Math.max(maxCount, count);
+			// reset the count
+			count = 1;
+		}
+	}
+	return maxCount;
 }
